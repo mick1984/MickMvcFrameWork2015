1e157082f919905008f4c27e5f2c532fc289baa8
mick1984 <mick_000@sina.com>
2018-10-16 19:27:29 +0800

    把另一个必要的项目整合进来

---
+++ MisBase2015.DataAccess/Users/RoleRight.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MisBase.Users
+{
+    /// <summary>
+    /// 角色对页面的访问权限类
+    /// </summary>
+    [Table("RoleRight")]
+    public class RoleRight
+    {
+        /// <summary>
+        /// 角色对页面访问权限ID
+        /// </summary>
+        [Key, Column(Order = 1)]
+        [StringLength(36)]
+        public string RightID
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        [Key, Column(Order = 2)]
+        [StringLength(36)]
+        public string RoleID
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [StringLength(500)]
+        public string RoleRightDesc
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        public RoleRight()
+        {
+        }
+
+        /// <summary>
+        /// 设置数据
+        /// </summary>
+        /// <param name="rightID"></param>
+        /// <param name="roleID"></param>
+        /// <param name="roleRightDesc"></param>
+        public void SetData(string rightID, string roleID, string roleRightDesc)
+        {
+            this.RoleRightDesc = roleRightDesc;
+            this.RoleID = roleID;
+            this.RightID = rightID;
+        }
+    }
+}
---
+++ MisBase2015.DataAccess/Users/Roles.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MisBase.Users
+{
+    /// <summary>
+    /// 管理角色的类
+    /// </summary>
+    public class Roles
+    {
+        /// <summary>
+        /// 角色的唯一键
+        /// </summary>     
+        [Key]
+        [StringLength(36)]
+        public string RoleID
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 角色的名称
+        /// </summary>      
+        [Unique]
+        [Required]
+        [StringLength(100)]
+        public string RoleName
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 角色的等级
+        /// </summary>
+        public int RoleLevel
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 角色描述
+        /// </summary>
+        [StringLength(500)]
+        public string RoleDesc
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        public Roles()
+        {
+        }
+
+        /// <summary>
+        /// 设置数据
+        /// </summary>
+        /// <param name="roleID"></param>
+        /// <param name="roleName"></param>
+        /// <param name="roleLevel"></param>
+        /// <param name="roleDesc"></param>
+        public void SetData(string roleID, string roleName, int roleLevel, string roleDesc)
+        {
+            this.RoleID = roleID;
+            this.RoleDesc = roleDesc;
+            this.RoleLevel = roleLevel;
+            this.RoleName = roleName;
+        }
+    }
+}
+
